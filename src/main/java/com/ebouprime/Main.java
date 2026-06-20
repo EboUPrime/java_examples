1812fe5096ff4f1a55da6ccac7617566893623a7
Givara <Givara.ebo@gmail.com>
2026-06-20 19:05:00 +0200

    Add user creation endpoint and example HTML form for API interaction

--- src/main/java/com/ebouprime/Main.java
+++ src/main/java/com/ebouprime/Main.java
@@ -11,21 +11,51 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> list = new ArrayList<>();
-
+        list.add(new User(1, "ebouprime", "info@ebouprime.com"));
 
         Javalin app = Javalin.create(config -> {
             RoutesConfig routes = config.routes;
+            StringBuilder homepage = new StringBuilder();
+            homepage.append("REST API with Javalin");
+            // Beispiel des HTTP-Statuscodes 200
+            homepage.append("\n");
+            homepage.append("HTTP-Codes");
+            homepage.append("\n");
+            homepage.append("200 OK");
+            homepage.append("\nhttp://localhost:7070/users");
+            homepage.append("\nThis Example fetches all users from the API");
+            homepage.append("\n");
+            homepage.append("201 Created");
+            homepage.append("\nhttp://localhost:7070/user?id=1&name=givara&email=test@ebouprime.com");
+
             routes.get("/", ctx -> {
-                ctx.result("REST API with Javalin");
+                ctx.result(homepage.toString());
             });
             routes.get("/users", ctx -> {
                 StringBuilder builder = new StringBuilder();
-                list.forEach(u -> builder.append(u.getName()).append("; "));
+                list.forEach(u -> {
+                    builder.append(u.getId()).append(" - ").append(u.getName()).append(" - ").append(u.getEmail()).append("\n");
+                });
+                ctx.result(builder.toString());
             });
             routes.get("/user/{id}", ctx -> {
                 String id = ctx.pathParam("id");
                 User user = new User(Integer.parseInt(id), "User " + id, "user" + id + "@example.com");
                 ctx.json(user);
+            });
+            routes.post("/user", ctx -> {
+                String body = ctx.body();
+                String[] parts = body.split("&");
+                String id = parts[0].split("=")[1];
+                String name = parts[1].split("=")[1];
+                String email = parts[2].split("=")[1];
+                User user = new User(Integer.parseInt(id), name, email);
+                list.add(user);
+                ctx.status(201); // Status 201
+                StringBuilder result = new StringBuilder();
+                result.append("Created user with id ").append(id).append("\n");
+                result.append("See all Users: http://localhost:7070/users");
+                ctx.result(result.toString());
             });
         }).start(7070);
 
